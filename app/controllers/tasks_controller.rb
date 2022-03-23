4260f189fa41053d96aa2503d481447240aa33f6
mei41 <oniku.1200tax@gmail.com>
2022-03-23 11:01:36 +0900

    new,createの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,15 @@
 class TasksController < ApplicationController
+    
     def new
         @task = Task.new
+    end
+
+    def create
+        Task.create(user_params)
+    end
+
+    private
+    def user_params
+        params.require(:task).permit(:title, :contents)
     end
 end
