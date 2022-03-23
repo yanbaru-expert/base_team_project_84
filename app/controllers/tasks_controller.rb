741c4aaf3a9b84924236d0b6e8f8e9ada74a4e1d
mei41 <oniku.1200tax@gmail.com>
2022-03-23 15:46:46 +0900

    destroyの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,11 @@ class TasksController < ApplicationController
         @task = Task.find(params[:id])
     end
 
+    def destoroy
+        task = Task.find(params[:id])
+        task.destoroy
+    end
+
     private
     def task_params
         params.require(:task).permit(:title, :contents)
