466695da3cafe11ecfff828b343def3e4fd29095
Aratahead <maramhisa.1229@gmail.com>
2022-04-04 14:25:05 +0900

    新規投稿機能実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+  def message_params
+    params.require(:message).permit(:titile, :content)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_011536) do
+ActiveRecord::Schema.define(version: 2022_04_03_061914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
